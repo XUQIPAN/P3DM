1cdd6aabafd2862cee521ff4b468779d786e9881
XUQIPAN <1668112439@qq.com>
2023-09-04 11:36:12 -0400

    Revert "parameter tuning"
    This reverts commit 2d3a348de609e470a531326a602b9a2d857a5e04.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # +
-export EXP_NAME=RANDOM_0814_CELEBA_EXP_EXP10_K256_IMAGE_SIZE_28_BATCH_128_IMPROVED_3
+export EXP_NAME=RANDOM_0116_CELEBA_EXP_EXP10_K256_IMAGE_SIZE_28_BATCH_128_IMPROVED_2
 # export CUDA_VISIBLE_DEVICES=4, 3, 5, 6, 7;
 python main.py --config celeba.yml --doc $EXP_NAME --exp /data/local/qipan/exp_celeba
 
